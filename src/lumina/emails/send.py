e8065bd8d6e91db208dd36cf83badd223a1665fd
Will Pimblett <will@wjdp.uk>
2022-01-12 01:49:09 +0000

    Implement email sending

---
+++ src/lumina/emails/send.py
@@ -0,0 +1,75 @@
+import email.utils
+import functools
+import logging
+from typing import List, NamedTuple, Optional, Tuple
+
+import boto3
+from botocore.exceptions import ClientError
+from mypy_boto3_ses import SESClient
+from mypy_boto3_ses.type_defs import BodyTypeDef, ContentTypeDef, MessageTypeDef
+
+log = logging.getLogger(__name__)
+
+FROM_ADDRESS = "nthp@wjdp.uk"
+CHARSET = "UTF-8"
+
+
+@functools.lru_cache
+def get_ses_client() -> SESClient:
+    return boto3.client("ses")
+
+
+class EmailError(Exception):
+    pass
+
+
+class EmailAddressUnverified(Exception):
+    pass
+
+
+class EmailBody(NamedTuple):
+    plaintext: str
+    html: str
+
+
+def send_email(
+    to_addresses: List[Tuple[Optional[str], str]],
+    subject: str,
+    body: EmailBody,
+) -> str:
+    """
+    Send an email via SES.
+    :param to_addresses: A list of email addresses to send the email to, tuples in the
+                         form (name, email). Name is optional.
+    :param subject: The subject of the email.
+    :param plaintext: The plaintext body of the email.
+    :param html: The HTML body of the email, optional.
+    :return: The message ID of the email.
+    """
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = get_ses_client().send_email(
+            Destination={
+                "ToAddresses": list(map(email.utils.formataddr, to_addresses)),
+            },
+            Message=MessageTypeDef(
+                Subject=ContentTypeDef(Data=subject, Charset=CHARSET),
+                Body=BodyTypeDef(
+                    Text=ContentTypeDef(Data=body.plaintext, Charset=CHARSET),
+                    Html=ContentTypeDef(Data=body.html, Charset=CHARSET),
+                ),
+            ),
+            Source=FROM_ADDRESS,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        error_message = e.response.get("Error", {}).get("Message", "")
+        log.error("Failed to send email: %s", error_message)
+        if "Email address not verified" in error_message:
+            raise EmailAddressUnverified() from e
+        raise EmailError() from e
+    else:
+        log.info("Email sent, message ID %s", response["MessageId"])
+        return response["MessageId"]
---
+++ tests/unit/lumina/emails/test_send.py
@@ -0,0 +1,33 @@
+import boto3
+import moto
+import pytest
+
+from lumina.emails.send import (
+    FROM_ADDRESS,
+    EmailAddressUnverified,
+    EmailBody,
+    send_email,
+)
+
+
+class TestSendEmail:
+    @moto.mock_ses
+    def test_success(self):
+        ses_client = boto3.client("ses")
+        to_address = "test@example.com"
+        ses_client.verify_email_identity(EmailAddress=FROM_ADDRESS)
+        send_email(
+            [(None, to_address)],
+            "Test Subject",
+            EmailBody(plaintext="Hello World", html="<p>Hello World</p>"),
+        )
+
+    @moto.mock_ses
+    def test_unverified_email(self):
+        to_address = "test@example.com"
+        with pytest.raises(EmailAddressUnverified):
+            send_email(
+                [(None, to_address)],
+                "Test Subject",
+                EmailBody(plaintext="Hello World", html="<p>Hello World</p>"),
+            )
